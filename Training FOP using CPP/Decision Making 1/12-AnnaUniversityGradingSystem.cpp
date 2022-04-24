4c8ac8861d4b850e640c854569c89f0757d23e54
Vishal Pandey <bishalpandey2001@gmail.com>
2022-04-24 12:33:59 +0530

    Create 12-AnnaUniversityGradingSystem.cpp

---
+++ Training FOP using CPP/Decision Making 1/12-AnnaUniversityGradingSystem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define lld long long double
+#define pb push_back
+#define ff front
+#define ss second
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define MOD 1e7+9
+
+void set_IO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main(){
+    set_IO();
+    return 0;
+}
