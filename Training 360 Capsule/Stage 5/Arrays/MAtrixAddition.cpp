2eff29f078ff02ac869a61dafb7a688294da643e
Vishal Pandey <bishalpandey2001@gmail.com>
2022-05-02 12:48:13 +0530

    Create MAtrixAddition.cpp

---
+++ Training 360 Capsule/Stage 5/Arrays/MAtrixAddition.cpp
@@ -0,0 +1,41 @@
+/*
+Ashok is a ninth-grade schoolboy. Miss Sumaiya is a very strict teacher and she teaches math to all the students. On the first day of the academic year, Miss Sumaiya gave him homework to find the addition of two matrices. Can you help Ashok by writing a program for the same.
+Input Format:
+
+The first input contains an integer 'r' which denotes the number of rows
+
+The second input contains an integer 'c' which denotes the number of columns
+
+The remaining input denotes the inputs of matrix1 and matrix2
+
+Output Format:
+
+Print the addition of two matrices
+
+Sample Input:
+
+2
+
+2
+
+2 3
+
+4 5
+
+4 5
+
+6 7
+
+Sample Output:
+
+6 8
+
+10 12
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    return 0;
+}
