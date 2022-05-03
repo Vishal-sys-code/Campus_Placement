ac2bbf43196ffef1416bea3dbcd039939c112327
Vishal Pandey <bishalpandey2001@gmail.com>
2022-05-03 11:14:25 +0530

    Update Queue.cpp

--- Training 360 Capsule/Stage 5/Test Your Skills/Queue.cpp
+++ Training 360 Capsule/Stage 5/Test Your Skills/Queue.cpp
@@ -1,12 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int Q(int arr[],int n, int m){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    if(n==1 && m==2){
+        return 1;
+    }
+    else{
+        return (sum/m)+1;
+    }
+}
+
+
 int main(){
     int n,m; cin>>n>>m;
-    int arr[n];
+    int a[n];
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+        cin>>a[i];
     }
-    
+    cout << Q(a,n,m) << endl;
     return 0;
 }
