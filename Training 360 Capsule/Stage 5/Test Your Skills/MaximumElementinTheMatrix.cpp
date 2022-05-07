3bd499b4752cae1bea275e033802987bb67ab4f6
Vishal Pandey <bishalpandey2001@gmail.com>
2022-05-07 09:17:20 +0530

    Create MaximumElementinTheMatrix.cpp

---
+++ Training 360 Capsule/Stage 5/Test Your Skills/MaximumElementinTheMatrix.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int r,c; cin>>r>>c;
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int max = arr[0][0];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(max < arr[i][j]){
+                max = arr[i][j];
+            }
+        }
+    }
+    cout << "The maximum element is " << max << endl;
+    return 0;
+}
