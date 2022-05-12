a11a718aaa3a9963f4f55730b682cdf8089d0852
Vishal Pandey <bishalpandey2001@gmail.com>
2022-05-12 08:07:01 +0530

    Create Nursery_Admission.cpp

---
+++ Training 360 Capsule/Stage 6/Test Your Skill/Nursery_Admission.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 50
+
+int main(){
+    string s;
+    cin>>s;
+    int len = s.length();
+    cout << "The number of letters in the name is " << len << endl;
+    return 0;
+}
