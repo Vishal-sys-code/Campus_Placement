6477a11f4aa7ecd58cb830699b3728e407fda607
Vishal Pandey <bishalpandey2001@gmail.com>
2022-05-15 07:42:09 +0530

    Update Student_Details.cpp

---
+++ Training 360 Capsule/Stage 7/Test Your Skills/Student_Details.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Student{ 
+    char name[30]; 
+    char department[20]; 
+    int yearOfStudy; 
+    float cgpa; 
+}; 
+
+int main(){
+    
+    return 0;
+}
